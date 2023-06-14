7d3c7bdbdb4c7359b63894d181f9357adcc208d5
Eeren <A300593@santanderrio.com.ar>
2023-06-13 21:14:29 -0300

    PrimierCommit -Creacion del Proyecto

---
+++ src/main/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplication.java
@@ -0,0 +1,13 @@
+package eggporIzquierda.solucionesactivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucionesactivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucionesactivasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplicationTests.java
@@ -0,0 +1,13 @@
+package eggporIzquierda.solucionesactivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucionesactivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
