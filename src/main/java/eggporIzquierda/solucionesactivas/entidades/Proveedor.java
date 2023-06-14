94393196962d49497861b7bc159168fc35f65479
analog-heart <hpeeren85@gmail.com>
2023-06-14 16:55:07 -0300

    PruebadesdebranchHernan

---
+++ src/main/java/eggporIzquierda/solucionesactivas/entidades/Proveedor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eggporIzquierda.solucionesactivas.entidades;
+
+/**
+ *
+ * @author A300593
+ */
+public class Proveedor {
+    
+}
