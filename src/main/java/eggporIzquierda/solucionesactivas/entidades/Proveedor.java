5bac67555ae77c3ea4322f37bbf8c5d4bccb2ef7
Lucio Coudannes <112908606+LucioCoudannes@users.noreply.github.com>
2023-06-14 21:13:11 -0300

    borre comenraios

--- src/main/java/eggporIzquierda/solucionesactivas/entidades/Proveedor.java
+++ src/main/java/eggporIzquierda/solucionesactivas/entidades/Proveedor.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package eggporIzquierda.solucionesactivas.entidades;
 
 /**
  *
- * @author A300593
+ * @author Hernan 
  */
 public class Proveedor {
     
