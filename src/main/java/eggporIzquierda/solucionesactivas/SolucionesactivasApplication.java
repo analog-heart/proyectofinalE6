84178846af86a14996771f26552d7272a2cc603b
brianezm <brianezm@gmail.com>
2023-06-24 18:27:36 -0300

    prueba 3 24 6 2023

--- src/main/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplication.java
+++ src/main/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplication.java
@@ -1,6 +1,6 @@
 package eggporIzquierda.solucionesactivas;
 
-import eggporIzquierda.solucionesactivas.service.ServicioContrato;
+//import eggporIzquierda.solucionesactivas.service.ServicioContrato;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +9,11 @@ public class SolucionesactivasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolucionesactivasApplication.class, args);
-                
-                
-                ServicioContrato sc = new ServicioContrato();
-                sc.crearContrato("11de24e3-5a1f-4b21-8a4f-5e87460dce1d", "13182323-cf12-4e65-90e1-24b7cb6c3a6c");
-                
+//                
+//                
+//                ServicioContrato sc = new ServicioContrato();
+//                sc.crearContrato("11de24e3-5a1f-4b21-8a4f-5e87460dce1d", "13182323-cf12-4e65-90e1-24b7cb6c3a6c");
+//                
           }
         
        }
