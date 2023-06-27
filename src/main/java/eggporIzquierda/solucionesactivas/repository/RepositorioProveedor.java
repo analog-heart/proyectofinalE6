5db10e7d7c0a680b107c5da911039ee30cf9814a
hhvc <hectorvazquez.laboral@gmail.com>
2023-06-26 22:34:56 -0300

    borrador

--- src/main/java/eggporIzquierda/solucionesactivas/repository/RepositorioProveedor.java
+++ src/main/java/eggporIzquierda/solucionesactivas/repository/RepositorioProveedor.java
@@ -13,8 +13,8 @@ public interface RepositorioProveedor extends JpaRepository<Proveedor, String> {
 
     @Query("SELECT u FROM Usuario u WHERE u.email = :email")
     public Proveedor buscarPorEmail(@Param("email") String email);
-    
-        @Query("SELECT u FROM Usuario u WHERE u.rol =:rol")
-    public List<Usuario> buscarPorNombre(@Param("rol") String rol);
+
+//    @Query("SELECT u FROM Usuario u WHERE u.dtype =:Proveedor")
+//    public List<Proveedor> buscarPorNombre(@Param("Proveedor") String proveedor);
 
 }
