8a17091f6e2917221e6de335b3ee2b3878e7da72
hhvc <hectorvazquez.laboral@gmail.com>
2023-07-01 17:33:48 -0300

    agregamos contactar proveedor

--- src/main/java/eggporIzquierda/solucionesactivas/controladores/ControladorProveedor.java
+++ src/main/java/eggporIzquierda/solucionesactivas/controladores/ControladorProveedor.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,8 +33,8 @@ public class ControladorProveedor {
         return "resultado_busqueda.html";
     }
 
-    @GetMapping("/contacto/{id}/")
-    public String contacto(String id, ModelMap modelo) {
+    @GetMapping("/{id}")
+    public String contacto(@PathVariable String id, ModelMap modelo) {
         Proveedor proveedor = proveedorServicio.getOne(id);
         modelo.addAttribute("proveedor", proveedor);
     return "proveedor_contratar.html";
