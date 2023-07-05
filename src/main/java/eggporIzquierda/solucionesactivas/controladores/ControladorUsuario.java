2f4321fd7d4cae6ff675e0788b1f0fdf2994c41a
Brian Emanuel Zapata Marin <brianezm@gmail.com>
2023-07-05 16:39:52 -0300

    avanzando con contratos y controlador proveedor (falta proveedor/mi_perfil_proveedor/modificar.html)

--- src/main/java/eggporIzquierda/solucionesactivas/controladores/ControladorUsuario.java
+++ src/main/java/eggporIzquierda/solucionesactivas/controladores/ControladorUsuario.java
@@ -3,6 +3,7 @@ package eggporIzquierda.solucionesactivas.controladores;
 import eggporIzquierda.solucionesactivas.entity.ContratoProveedor;
 import eggporIzquierda.solucionesactivas.entity.Usuario;
 import eggporIzquierda.solucionesactivas.exception.MiException;
+import eggporIzquierda.solucionesactivas.repository.RepositorioContrato;
 import eggporIzquierda.solucionesactivas.service.ServicioContrato;
 import eggporIzquierda.solucionesactivas.service.ServicioUsuario;
 import jakarta.servlet.http.HttpSession;
@@ -20,47 +21,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-
 @Controller
 @RequestMapping("/usuario")
-
 public class ControladorUsuario {
 
+    @Autowired
+    private RepositorioContrato repositorioContrato;
 
-    
     @Autowired
     private ServicioUsuario usuarioServicio;
-    
+
     @Autowired
     private ServicioContrato contratoServicio;
 
-    
     @GetMapping("/usuarios")
-    public String listarUsuariosActivos(ModelMap modelo){
+    public String listarUsuariosActivos(ModelMap modelo) {
         List<Usuario> usuarios = usuarioServicio.listarUsuariosActivos();
         modelo.addAttribute("usuarios", usuarios);
-       return "usuario_list.html";
+        return "usuario_list.html";
     }
-    
-    
 
     @GetMapping("/buscar")
-    public String buscarUsuarios(String nombre,ModelMap modelo) {
-         
+    public String buscarUsuarios(String nombre, ModelMap modelo) {
+
         List<Usuario> resultados = usuarioServicio.buscarUsuariosXnombre(nombre);
         modelo.addAttribute("resultados", resultados);
         return "resultado_busqueda.html";
     }
 
-  @PreAuthorize("hasAnyRole('ROLE_USUARIO', 'ROLE_ADMIN')")
-
+    @PreAuthorize("hasAnyRole('ROLE_USUARIO', 'ROLE_ADMIN')")
     @GetMapping("/modificar_perfil_usuario")
     public String perfil(ModelMap modelo, HttpSession session) {
 
         Usuario usuario = (Usuario) session.getAttribute("usuariosession");
         modelo.addAttribute("usuario", usuarioServicio.getOne(usuario.getId()));
         modelo.put("usuario", usuario);
-        
+
         return "usuario_modificar_v2.html";
 
     }
@@ -73,50 +69,44 @@ public class ControladorUsuario {
         try {
             usuarioServicio.actualizar(archivo, id, nombre, email, password, password2, nombreUsuario, apellido, fechaNacimiento, dni, telefono);
             modelo.put("exito", "Usuario actualizado correctamente!");
-            
+
             return "inicio.html";
-            
+
         } catch (MiException ex) {
             modelo.put("error", ex.getMessage());
             modelo.put("nombre", nombre);
             modelo.put("email", email);
-            
+
             return "usuario_modificar.html";
         }
 
     }
-    
-    
-    
+
     @GetMapping("/mi_perfil_usuario")
     public String mi_perfil_usuario(ModelMap modelo, HttpSession session) {
-        
+
         Usuario usuario = (Usuario) session.getAttribute("usuariosession");
         modelo.addAttribute("usuario", usuarioServicio.getOne(usuario.getId()));
-        modelo.put("usuario", usuario);
-        
+
         List<ContratoProveedor> contratosSesion = new ArrayList();
-        
+
         contratosSesion = contratoServicio.listarContratosSesion(usuario);
         modelo.put("contratosUsuario", contratosSesion);
-        
-        
+
+        List<ContratoProveedor> cantidadContratosSolicitados = repositorioContrato.listarPorEstadoSolicitado(usuario.getId());
+        modelo.put("cantidadContratosSolicitados", cantidadContratosSolicitados.size());
+
         return "mi_perfil_usuario.html";
 
     }
 
-    
-       
-      @GetMapping("/dar_baja_usuario")
+    @GetMapping("/dar_baja_usuario")
     public String dar_baja_usuario(ModelMap modelo, HttpSession session) {
 
         Usuario usuario = (Usuario) session.getAttribute("usuariosession");
         usuarioServicio.dar_baja_usuario(usuario.getId());
-        
-        
+
         return "mi_perfil_usuario.html";
 
-    }         
+    }
 }
-
-
