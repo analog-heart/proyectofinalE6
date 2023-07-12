818c5515d94d8beea331ffcc0edd65b20c420cb0
FabriBorsa <95548134+FabriBorsa@users.noreply.github.com>
2023-07-11 22:39:59 -0300

    ok

--- src/main/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplication.java
+++ src/main/java/eggporIzquierda/solucionesactivas/SolucionesactivasApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SolucionesactivasApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SolucionesactivasApplication.class, args);
 
