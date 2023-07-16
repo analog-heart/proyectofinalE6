162da281cb377915df3f7f564e5a3d3734684975
Brian Emanuel Zapata Marin <brianezm@gmail.com>
2023-07-16 02:22:09 -0300

    refactorizando ContratoProveedor por Contrato // Agregue atributo a entidad Contrato // Puliendo codigo // Iniciando th:if para vistas sin contratos, que arroje un msj 'no tenes contratos' //

--- src/main/java/eggporIzquierda/solucionesactivas/repository/RepositorioContrato.java
+++ src/main/java/eggporIzquierda/solucionesactivas/repository/RepositorioContrato.java
@@ -1,6 +1,6 @@
 package eggporIzquierda.solucionesactivas.repository;
 
-import eggporIzquierda.solucionesactivas.entity.ContratoProveedor;
+import eggporIzquierda.solucionesactivas.entity.Contrato;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,37 +8,33 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepositorioContrato extends JpaRepository<ContratoProveedor, String> {
+public interface RepositorioContrato extends JpaRepository<Contrato, String> {
 
-    // @Query("SELECT c FROM ContratoProveedor c WHERE c.estado.toString() = :estado
-    // AND c.proveedor.id =:idProveedor")
-    // public List<ContratoProveedor> listarPorEstado(@Param("estado") String
-    // estado, @Param("idProveedor") String idProveedor);
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'ENCURSO' AND c.proveedor.id =:idProveedor")
-    public List<ContratoProveedor> listarPorEstadoEncurso(@Param("idProveedor") String idProveedor);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'ENCURSO' AND c.proveedor.id =:idProveedor")
+    public List<Contrato> listarPorEstadoEncurso(@Param("idProveedor") String idProveedor);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'CALIFICADO' AND c.proveedor.id =:idProveedor")
-    public List<ContratoProveedor> listarPorEstadoCalificado(@Param("idProveedor") String idProveedor);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'CALIFICADO' AND c.proveedor.id =:idProveedor")
+    public List<Contrato> listarPorEstadoCalificado(@Param("idProveedor") String idProveedor);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'SOLICITADO' AND c.proveedor.id =:idProveedor")
-    public List<ContratoProveedor> listarPorEstadoSolicitado(@Param("idProveedor") String idProveedor);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'SOLICITADO' AND c.proveedor.id =:idProveedor")
+    public List<Contrato> listarPorEstadoSolicitado(@Param("idProveedor") String idProveedor);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'TERMINADO' AND c.proveedor.id =:idProveedor")
-    public List<ContratoProveedor> listarPorEstadoTerminado(@Param("idProveedor") String idProveedor);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'TERMINADO' AND c.proveedor.id =:idProveedor")
+    public List<Contrato> listarPorEstadoTerminado(@Param("idProveedor") String idProveedor);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.comentarioOfensivo = true")
-    public List<ContratoProveedor> listarPorEstadoDenunciado();
+    @Query("SELECT c FROM Contrato c WHERE c.comentarioDenunciado = true")
+    public List<Contrato> listarPorEstadoDenunciado();
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'ENCURSO' OR c.estado = 'SOLICITADO' AND c.usuario.id =:idUsuario")
-    public List<ContratoProveedor> listarPorEstadoEncursoCliente(@Param("idUsuario") String idUsuario);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'ENCURSO' OR c.estado = 'SOLICITADO' AND c.usuario.id =:idUsuario")
+    public List<Contrato> listarPorEstadoEncursoCliente(@Param("idUsuario") String idUsuario);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'CALIFICADO' AND c.usuario.id =:idUsuario")
-    public List<ContratoProveedor> listarPorEstadoCalificadoCliente(@Param("idUsuario") String idUsuario);
+    // @Query("SELECT c FROM Contrato c WHERE c.estado = 'CALIFICADO' AND c.usuario.id =:idUsuario")
+    // public List<Contrato> listarPorEstadoCalificadoCliente(@Param("idUsuario") String idUsuario);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'SOLICITADO' AND c.usuario.id =:idUsuario")
-    public List<ContratoProveedor> listarPorEstadoSolicitadoCliente(@Param("idUsuario") String idUsuario);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'SOLICITADO' AND c.usuario.id =:idUsuario")
+    public List<Contrato> listarPorEstadoSolicitadoCliente(@Param("idUsuario") String idUsuario);
 
-    @Query("SELECT c FROM ContratoProveedor c WHERE c.estado = 'TERMINADO' AND c.usuario.id =:idUsuario")
-    public List<ContratoProveedor> listarPorEstadoTerminadoCliente(@Param("idUsuario") String idUsuario);
+    @Query("SELECT c FROM Contrato c WHERE c.estado = 'TERMINADO' AND c.usuario.id =:idUsuario")
+    public List<Contrato> listarPorEstadoTerminadoCliente(@Param("idUsuario") String idUsuario);
 
 }
